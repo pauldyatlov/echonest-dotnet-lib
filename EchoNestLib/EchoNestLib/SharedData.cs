ad5939e4001608f609424437c4726e88146bd4ed
fabbri00 <fabbri00@e5159243-ce46-270b-c6d9-228fc8398174>
2010-11-05 15:09:50 +0000

    Initial Commit

---
+++ EchoNestLib/EchoNestLib/SharedData.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EchoNestLib.Properties;
+using System.Net;
+using System.IO;
+using SeasideResearch.LibCurlNet;
+
+namespace EchoNestLib
+{
+    public static class SharedData
+    {
+        /// <summary>
+        /// Returns the APIKey that is stored into settings
+        /// </summary>
+        public static string APIKey
+        {
+            get
+            {
+                return Settings.Default.APIKey;
+            }
+        }
+
+        public static Int32 OnWriteData(Byte[] buf, Int32 size, Int32 nmemb,
+        Object extraData)
+        {
+
+            PostRequestResult = System.Text.Encoding.UTF8.GetString(buf);
+            return size * nmemb;
+        }
+
+        private static string PostRequestResult;
+
+        /// <summary>
+        /// Returns the result and empties it
+        /// </summary>
+        /// <returns></returns>
+        public static string ReadPostRequestResult()
+        {
+            string res = PostRequestResult;
+            PostRequestResult = string.Empty;
+            return res;
+
+        }
+
+        public static string PerformPostMultipartRequest(string query, string postData)
+        {
+            Curl.GlobalInit((int)CURLinitFlag.CURL_GLOBAL_ALL);
+
+            Easy easy = new Easy();
+
+            Easy.WriteFunction wf = new Easy.WriteFunction(OnWriteData);
+            easy.SetOpt(CURLoption.CURLOPT_WRITEFUNCTION, wf);
+
+            
+            // simple post - with a string
+            easy.SetOpt(CURLoption.CURLOPT_POSTFIELDS,
+                postData);
+
+            easy.SetOpt(CURLoption.CURLOPT_USERAGENT,
+                "C# EchoNest Lib");
+            easy.SetOpt(CURLoption.CURLOPT_FOLLOWLOCATION, true);
+            easy.SetOpt(CURLoption.CURLOPT_URL,
+                query);
+            easy.SetOpt(CURLoption.CURLOPT_POST, true);
+
+           
+            CURLcode res = easy.Perform();
+
+            if (res != CURLcode.CURLE_OK)
+                throw new WebException("Post operation failed");
+    
+           
+            
+            easy.Cleanup();
+            string code = string.Empty;
+            
+
+
+            Curl.GlobalCleanup();
+            
+            
+            return string.Empty;
+
+            //ASCIIEncoding encoding = new ASCIIEncoding();
+            //string postData = string.Format("api_key={0}&url={1}&wait=true&format=xml", SharedData.APIKey, fileUrl);
+
+            //byte[] data = encoding.GetBytes(postData);
+
+            //// Prepare web request...
+            //HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(query);
+            //myRequest.Method = "POST";
+            //myRequest.UserAgent = "C# EchoNest Lib";
+
+            ////myRequest.ContentType = "multipart/form-data";
+            //myRequest.ContentLength = data.Length;
+            //Stream newStream = myRequest.GetRequestStream();
+            //// Send the data.
+            //newStream.Write(data, 0, data.Length);
+            //newStream.Close();
+
+
+            //HttpWebResponse response = null;
+
+            //string responseBody;
+
+            //int statusCode;
+
+
+            //try
+            //{
+
+            //    response = (HttpWebResponse)myRequest.GetResponse();
+            //    //byte[] buf = new byte[8192];
+            //    Stream respStream = response.GetResponseStream();
+            //    StreamReader respReader = new StreamReader(respStream);
+
+
+
+            //    responseBody = respReader.ReadToEnd();
+            //    statusCode = (int)(HttpStatusCode)response.StatusCode;
+
+            //}
+            //catch (WebException ex)
+            //{
+            //    response = (HttpWebResponse)ex.Response;
+            //    responseBody = "No Server Response";
+
+
+            //}
+
+            //return responseBody;
+        }
+
+        public static string PerformGetRequest(string query)
+        {
+
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(query);
+
+            request.Method = "GET";
+            request.UserAgent = "C# EchoNest Lib";
+            //request.ContentLength = 8192;
+            HttpWebResponse response = null;
+
+            string responseBody;
+
+            int statusCode;
+
+
+            try
+            {
+
+                response = (HttpWebResponse)request.GetResponse();
+                //byte[] buf = new byte[8192];
+                Stream respStream = response.GetResponseStream();
+                StreamReader respReader = new StreamReader(respStream);
+
+
+
+                responseBody = respReader.ReadToEnd();
+                statusCode = (int)(HttpStatusCode)response.StatusCode;
+
+            }
+            catch (WebException ex)
+            {
+                response = (HttpWebResponse)ex.Response;
+                responseBody = "No Server Response";
+
+
+            }
+
+            return responseBody;
+
+        }
+    }
+}
---
+++ EchoNestLib/EchoNestTestApp/Window1.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EchoNestTestApp
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            EchoNestLib.APIs.ArtistAPI.Instance.Search("electric sheep");
+            List<string> bu = new List<string>();
+
+            bu.Add("news");
+            bu.Add("reviews");
+
+            EchoNestLib.APIs.ArtistAPI.Instance.Search("radiohead", 1, EchoNestLib.APIs.ArtistAPI.ArtistSortType.HotttnesssDesc, true, bu);
+        }
+
+        private void Track_click(object sender, RoutedEventArgs e)
+        {
+            EchoNestLib.APIs.TrackAPI.Instance.Upload(string.Empty);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            EchoNestLib.APIs.TrackAPI.Instance.Analyze("TRMFRFJ12C1992D658", string.Empty);
+
+        }
+
+
+    }
+}
