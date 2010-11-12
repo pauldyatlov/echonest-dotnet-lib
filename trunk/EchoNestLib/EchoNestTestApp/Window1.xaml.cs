96f0d0074744ef9633a1c511d8ff525e88979c1d
fabbri00 <fabbri00@e5159243-ce46-270b-c6d9-228fc8398174>
2010-11-12 15:24:36 +0000

    Test app update

--- trunk/EchoNestLib/EchoNestTestApp/Window1.xaml.cs
+++ trunk/EchoNestLib/EchoNestTestApp/Window1.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace EchoNestTestApp
+namespace ElectricSheep.EchoNestTestApp
 {
     /// <summary>
     /// Interaction logic for Window1.xaml
@@ -26,7 +26,7 @@ namespace EchoNestTestApp
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            EchoNestLib.APIs.ArtistAPI.Instance.Search("electric sheep");
+            ElectricSheep.EchoNestLib.APIs.ArtistAPI.Instance.Search("electric sheep");
             List<string> bu = new List<string>();
 
             bu.Add("news");
@@ -37,7 +37,7 @@ namespace EchoNestTestApp
 
         private void Track_click(object sender, RoutedEventArgs e)
         {
-            EchoNestLib.APIs.TrackAPI.Instance.Upload("http://www.theelectricsheep.com/working/Beginnin-MozartFull.mp3");
+            ElectricSheep.EchoNestLib.APIs.TrackAPI.Instance.Upload("http://www.theelectricsheep.com/working/Beginnin-MozartFull.mp3");
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
@@ -46,9 +46,19 @@ namespace EchoNestTestApp
             //EchoNestLib.APIs.TrackAPI.Instance.Analyze("TRMFRFJ12C1992D658", string.Empty); 
 
             //beginning
-            EchoNestLib.APIs.TrackAPI.Instance.Analyze("TRBIIJN12C1992D6C8", string.Empty);
+            ElectricSheep.EchoNestLib.APIs.TrackAPI.Instance.Analyze("TRBIIJN12C1992D6C8", string.Empty);
             
 
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            ElectricSheep.EchoNestLib.APIs.ArtistAPI.Instance.Audio("beatles", string.Empty, 40, 23);
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            double value = ElectricSheep.EchoNestLib.APIs.ArtistAPI.Instance.Familiarity("rolling stones", string.Empty);
         }
 
        
